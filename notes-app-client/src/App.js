0142d4a8652f3f355af3a23b2041e43716bbe271
ignacio <iggirex@gmail.com>
2018-03-23 16:55:58 -0600

    added LinkContainer to prevent refresh and do SPA

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Nav, Navbar, NavItem } from "react-bootstrap";
 import './App.css';
 import Routes from "./Routes";
 
@@ -15,16 +18,19 @@ class App extends Component {
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/signup">
+                <NavItem>Signup</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <NavItem>Login</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
         <Routes />
       </div>
-        // <header className="App-header">
-        //   <img src={logo} className="App-logo" alt="logo" />
-        //   <h1 className="App-title">Welcome to React</h1>
-        // </header>
-        // <p className="App-intro">
-        //   To get started, edit <code>src/App.js</code> and save to reload.
-        // </p>
     );
   }
 }
