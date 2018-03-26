3d4d40c0b49e3e5ba78a14f0e709a5d9cc28eab4
ignacio <iggirex@gmail.com>
2018-03-26 16:37:47 -0600

    serverless api working, posting to Dynamo thru CLI

---
+++ notes-app-client/notes-app-api/create.js
@@ -0,0 +1,48 @@
+import uuid from "uuid";
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: "us-west-2" });
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+export function main(event, context, callback) {
+    // Request body is passed in as a JSON encoded string in 'event.body'
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: "notes",
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: new Date().getTime()
+        }
+    };
+
+    dynamoDB.put(params, (error, data) => {
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        };
+
+        //return code 500 on error
+        if (error) {
+            const response = {
+                statusCode: 500,
+                headers: headers,
+                body: JSON.stringify({ status: false })
+            };
+            callback(null, response);
+            return;
+        }
+
+        //return code 200 and newly created item
+        const response = {
+            statusCode: 200,
+            headers: headers,
+            body: JSON.stringify(params.item)
+        };
+        callback(null, response);
+    });
+}
